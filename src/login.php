9f225f8533efde922ed0b674c9b7a96e608947a4
Darryl Ng Bing Bing <145664904+Darryl-SkyEzio@users.noreply.github.com>
2024-03-11 02:13:57 +0800

    feat: Loginpage
    fixed: dimension for Login Page
    
    feat: added validation for Loginpage, adjusted text for dynamic rescale and added some design animation
    
    sidenote: when to copied and paste the login.php to logout.php as placementholder

--- src/login.php
+++ src/login.php
@@ -12,22 +12,67 @@
 <body>
     <div class="container" id="container">
         <div class="form-container sign-up">
-            <form>
-                <h1>Create Account</h1>
-                <input type="text" placeholder="Name">
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <input type="password" placeholder="Confirm Password">
-                <button>Sign Up</button>
+            <form action="process_register.php" method="post">
+                <h1>
+                    <span>C</span><span>r</span><span>e</span><span>a</span><span>t</span><span>e</span>
+                    <span> </span>
+                    <span>A</span><span>c</span><span>c</span><span>o</span><span>u</span><span>n</span><span>t</span>
+                </h1>
+
+                <div class="mb-3">
+                    <label for="fname" class="form-label"></label>
+                    <input required maxlength="45" type="text" id="fname" name="fname" class="form-control"
+                        placeholder="Enter first name">
+                </div>
+                <div class="mb-3">
+                    <label for="lname" class="form-label"></label>
+                    <input required maxlength="45" type="text" id="lname" name="lname" class="form-control"
+                        placeholder="Enter last name">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label"></label>
+                    <input required type="email" id="email" name="email" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label"></label>
+                    <input required maxlength="30" type="password" id="pwd" name="pwd" class="form-control"
+                        placeholder="Enter password">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd_confirm" class="form-label"></label>
+                    <input required maxlength="30" type="password" id="pwd_confirm" name="pwd_confirm"
+                        class="form-control" placeholder="Confirm password">
+                </div>
+                <div class="mb-3 form-check">
+                    <input required type="checkbox" name="agree" id="agree" class="form-check-input">
+                    <label class="form-check-label" for="agree">
+                        Agree to terms and conditions.
+                    </label>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
         <div class="form-container sign-in">
-            <form>
-                <h1>Welcome Back</h1>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="Password">
-                <a href="#">Forget Your Password?</a>
-                <button>Sign In</button>
+            <form action="process_login.php" method="post" class="login-form">
+                <h1>
+                    <span>W</span><span>e</span><span>l</span><span>c</span><span>o</span><span>m</span><span>e</span>
+                    <span> </span>
+                    <span>B</span><span>a</span><span>c</span><span>k</span>
+                </h1>
+                <div class="mb-3">
+                    <label for="email" class="form-label"></label>
+                    <input required type="email" id="email" name="email" class="form-control" placeholder="Email">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label"></label>
+                    <input required maxlength="30" type="password" id="pwd" name="pwd" class="form-control"
+                        placeholder="Password">
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
         <div class="toggle-container">
--- src/logout.php
+++ src/logout.php
@@ -1,26 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Our Project</title>
-    </head>
-    <body>
-        <header>
-            <h1>This the LOGOUT PAGE</h1>
-        </header>
-        <main>
-            <h2>This the main</h2>
-            <h3>Test routing</h3>
-            <ul>
-                <li>Link to home page: <a href="index.html">Home page</a></li>
-                <li>Link to login page: <a href="login.html">Login page</a></li>
-                <li>Link to register page: <a href="register.html">Register page</a></li>
-                <li>Link to logout page: <a href="logout.html">Logout page</a></li>
-                <li>Link to about page: <a href="about.html">About page</a></li>
-                <li>Link to blog page: <a href="blog.html">Blog page</a></li>
-            </ul>
-        </main>
-        <footer>
-            <h2>This the footer</h2>
-        </footer>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="asset/css/login.css">
+    <title>TravelTalk LoginPage</title>
+</head>
+
+<body>
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Create Account</h1>
+                <input type="text" placeholder="Name">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <input type="password" placeholder="Confirm Password">
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form>
+                <h1>Welcome Back</h1>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <a href="#">Forget Your Password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Traveller!</h1>
+                    <p>Not Registered? Register Now with your personal details to use all of site features</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
