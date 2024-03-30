800aaa9972a67336123a956fc86ff303e14171e0
fishsoupp <limjingyu120@gmail.com>
2024-03-31 04:36:30 +0800

    styling for package.php

--- src/package.php
+++ src/package.php
@@ -9,8 +9,8 @@
 		<script defer src="/asset/js/main.js"></script>
 
 		<!-- Page JS -->
-		<link rel="stylesheet" href="/asset/css/about.css" />
-		<script defer src="/asset/js/about.js"></script>
+		<link rel="stylesheet" href="/asset/css/package.css" />
+		<script defer src="/asset/js/package.js"></script>
 
 		<!--Bootstrap JS-->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
@@ -30,7 +30,6 @@
 		<?php
 
       include "component/header.component.php";
-      include "component/nav.component.php";
       include "component/landing-screen.component.php";
 
       // Load database config
